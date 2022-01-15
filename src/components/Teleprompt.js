0b3b2ca535a0490899412b5c3d444ef66775597e
Jonnycp <jonathan-caputo@hotmail.com>
2022-01-15 01:54:00 +0100

    Teleprompter statico
    Leggi la frase, se riesci
    Da fixare responsive

--- src/components/Teleprompt.js
+++ src/components/Teleprompt.js
@@ -1,8 +1,13 @@
 import React from "react";
+import styles from "../styles/teleprompt.module.css"
 
 const Teleprompt = () => {
     return ( 
-        <></>
+        <div className={styles.prompt}>
+            Prova di lettura dal GOBBO. <br/>
+            Se riesci a leggermi significa che sai leggere. <br/>
+            Sembra una magia ma in realtà è proprio così!😲
+        </div>
     );
 }
 
