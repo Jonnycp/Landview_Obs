c7675acc0bea58e7046384a2aea1b78bb34182ae
Jonnycp <jonathan-caputo@hotmail.com>
2022-01-15 01:52:17 +0100

    Creato viewer live RP (statico)
    Sostituire se possibile, con il flusso video NDI sulla rete locale

---
+++ src/components/Viewer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "../styles/viewer.module.css";
+
+
+function Viewer({title}){
+    return <div className={styles.viewer}>
+        <h2>{title}</h2>
+        <iframe src="https://player.twitch.tv/?channel=radiopanettibari&muted=true&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no" height="348" width="620" title={title}></iframe>
+    </div>
+}
+
+export default Viewer;
